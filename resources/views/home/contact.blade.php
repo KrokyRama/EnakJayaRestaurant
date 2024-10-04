9b3500c791e5bcef2a76261e295da587953e59d1
aufafahmish <aufafahmi25@gmail.com>
2024-10-04 19:01:52 +0700

    commitfahmi1
    header footer update

--- resources/views/home/contact.blade.php
+++ resources/views/home/contact.blade.php
@@ -1,49 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="Responsive Bootstrap4 Shop Template, Created by Imran Hossain from https://imransdesign.com/">
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
-
-    <!-- title -->
-    <title>Contact</title>
-
-    <!-- favicon -->
-    <link rel="shortcut icon" type="image/png" href="assets/img/favicon.png">
-    <!-- google font -->
-    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css?family=Poppins:400,700&display=swap" rel="stylesheet">
-    <!-- fontawesome -->
-    <link rel="stylesheet" href="assets/css/all.min.css">
-    <!-- bootstrap -->
-    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
-    <!-- owl carousel -->
-    <link rel="stylesheet" href="assets/css/owl.carousel.css">
-    <!-- magnific popup -->
-    <link rel="stylesheet" href="assets/css/magnific-popup.css">
-    <!-- animate css -->
-    <link rel="stylesheet" href="assets/css/animate.css">
-    <!-- mean menu css -->
-    <link rel="stylesheet" href="assets/css/meanmenu.min.css">
-    <!-- main style -->
-    <link rel="stylesheet" href="assets/css/main.css">
-    <!-- responsive -->
-    <link rel="stylesheet" href="assets/css/responsive.css">
-
-</head>
-<body>
-
-<!--PreLoader-->
-<!-- <div class="loader">
-    <div class="loader-inner">
-        <div class="circle"></div>
-    </div>
-</div> -->
-<!--PreLoader Ends-->
-
-<!-- header -->
+@extends('home.headerfooter')
+@section('content')
 <div class="top-header-area" id="sticker">
     <div class="container">
         <div class="row">
@@ -105,7 +61,7 @@
         </div>
     </div>
 </div>
-<!-- end search arewa -->
+<!-- end search area -->
 
 <!-- breadcrumb-section -->
 <div class="breadcrumb-section breadcrumb-bg">
@@ -128,39 +84,39 @@
         <div class="row">
             <div class="col-lg-8 mb-5 mb-lg-0">
                 <div class="form-title">
-                    <h2>Have you any question?</h2>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur, ratione! Laboriosam est, assumenda. Perferendis, quo alias quaerat aliquid. Corporis ipsum minus voluptate? Dolore, esse natus!</p>
+                    <h2>Punya Keluhan atau Saran?</h2>
+                    <p>Sampaikan keluhan atau saranmu untuk kami di sini! Kami akan dengan senang hati menerimanya.</p>
                 </div>
                 <div id="form_status"></div>
                 <div class="contact-form">
                     <form type="POST" id="fruitkha-contact" onSubmit="return valid_datas( this );">
                         <p>
-                            <input type="text" placeholder="Name" name="name" id="name">
+                            <input type="text" placeholder="Nama" name="name" id="name">
                             <input type="email" placeholder="Email" name="email" id="email">
                         </p>
                         <p>
-                            <input type="tel" placeholder="Phone" name="phone" id="phone">
+                            <input type="tel" placeholder="No Telepon" name="phone" id="phone">
                             <input type="text" placeholder="Subject" name="subject" id="subject">
                         </p>
-                        <p><textarea name="message" id="message" cols="30" rows="10" placeholder="Message"></textarea></p>
+                        <p><textarea name="message" id="message" cols="30" rows="10" placeholder="Pesan"></textarea></p>
                         <input type="hidden" name="token" value="FsWga4&@f6aw" />
-                        <p><input type="submit" value="Submit"></p>
+                        <p><input type="submit" value="Kirim"></p>
                     </form>
                 </div>
             </div>
             <div class="col-lg-4">
                 <div class="contact-form-wrap">
                     <div class="contact-form-box">
-                        <h4><i class="fas fa-map"></i> Shop Address</h4>
-                        <p>34/8, East Hukupara <br> Gifirtok, Sadan. <br> Country Name</p>
+                        <h4><i class="fas fa-map"></i> Alamat</h4>
+                        <p>Jl. Dr. Ir. H. Soekarno 60115 <br> Mulyorejo, Surabaya. <br> Indonesia</p>
                     </div>
                     <div class="contact-form-box">
-                        <h4><i class="far fa-clock"></i> Shop Hours</h4>
-                        <p>MON - FRIDAY: 8 to 9 PM <br> SAT - SUN: 10 to 8 PM </p>
+                        <h4><i class="far fa-clock"></i> Jam Buka</h4>
+                        <p>SENIN - JUMAT: 09.00 - 23.00 WIB <br> SABTU - MINGGU: 10.00 - 24.00 WIB </p>
                     </div>
                     <div class="contact-form-box">
-                        <h4><i class="fas fa-address-book"></i> Contact</h4>
-                        <p>Phone: +00 111 222 3333 <br> Email: support@fruitkha.com</p>
+                        <h4><i class="fas fa-address-book"></i> Kontak</h4>
+                        <p>Phone: 326032 <br> Email: enakjayasupport@gmail.com</p>
                     </div>
                 </div>
             </div>
@@ -174,7 +130,7 @@
     <div class="container">
         <div class="row">
             <div class="col-lg-12 text-center">
-                <p> <i class="fas fa-map-marker-alt"></i> Find Our Location</p>
+                <p> <i class="fas fa-map-marker-alt"></i> Cek Alamat Kami</p>
             </div>
         </div>
     </div>
@@ -186,76 +142,3 @@
     <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1176.6560427515647!2d112.78459867894043!3d-7.2654740089699!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dd7fb7a9bdf4517%3A0x5b8bbf8317f96ed5!2sGedung%20Kuliah%20Bersama%20(GKB)%20Kampus%20C%20Unair!5e0!3m2!1sid!2sid!4v1726999652001!5m2!1sid!2sid" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen="" class="embed-responsive-item"></iframe>
 </div>
 <!-- end google map section -->
-
-
-<!-- footer -->
-<div class="footer-area">
-    <div class="container">
-        <div class="row">
-            <div class="col-lg-3 col-md-6">
-                <div class="footer-box about-widget">
-                    <h2 class="widget-title">About us</h2>
-                    <p>Ut enim ad minim veniam perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae.</p>
-                </div>
-            </div>
-            <div class="col-lg-3 col-md-6">
-                <div class="footer-box get-in-touch">
-                    <h2 class="widget-title">Get in Touch</h2>
-                    <ul>
-                        <li>34/8, East Hukupara, Gifirtok, Sadan.</li>
-                        <li>support@fruitkha.com</li>
-                        <li>+00 111 222 3333</li>
-                    </ul>
-                </div>
-            </div>
-            <div class="col-lg-3 col-md-6">
-                <div class="footer-box pages">
-                    <h2 class="widget-title">Pages</h2>
-                    <ul>
-                        <li><a href="{{ url('/')}}">Home</a></li>
-                        <li><a href="{{ url('/shop') }}">Shop</a></li>
-                        <li><a href="{{ url('/contact') }}">Contact</a></li>
-                    </ul>
-                </div>
-            </div>
-            <div class="col-lg-3 col-md-6">
-                <div class="footer-box subscribe">
-                    <h2 class="widget-title">Subscribe</h2>
-                    <p>Subscribe to our mailing list to get the latest updates.</p>
-                    <form action="index.html">
-                        <input type="email" placeholder="Email">
-                        <button type="submit"><i class="fas fa-paper-plane"></i></button>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-<!-- end footer -->
-
-
-<!-- jquery -->
-<script src="assets/js/jquery-1.11.3.min.js"></script>
-<!-- bootstrap -->
-<script src="assets/bootstrap/js/bootstrap.min.js"></script>
-<!-- count down -->
-<script src="assets/js/jquery.countdown.js"></script>
-<!-- isotope -->
-<script src="assets/js/jquery.isotope-3.0.6.min.js"></script>
-<!-- waypoints -->
-<script src="assets/js/waypoints.js"></script>
-<!-- owl carousel -->
-<script src="assets/js/owl.carousel.min.js"></script>
-<!-- magnific popup -->
-<script src="assets/js/jquery.magnific-popup.min.js"></script>
-<!-- mean menu -->
-<script src="assets/js/jquery.meanmenu.min.js"></script>
-<!-- sticker js -->
-<script src="assets/js/sticker.js"></script>
-<!-- form validation js -->
-<script src="assets/js/form-validate.js"></script>
-<!-- main js -->
-<script src="assets/js/main.js"></script>
-
-</body>
-</html>
